5a245d1c8f46397d3ff51f085111d9a5c2b8b793
Happ(김복현) <bok@coupang.com>
2016-11-18 00:19:33 +0900

    improved

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+	console.debug("chrome.tabs.onUpdated.listener "+tab.url);
+});
+
 chrome.browserAction.onClicked.addListener(function(tab){
 
 	//chrome.downloads.setShelfEnabled(false);
--- getSource.js
+++ getSource.js
@@ -1,6 +1,5 @@
 function get_source(document_body, document){
 
-//	document.querySelector(".logo").innerText("ABCD");
 
     return document_body.innerText;
 }
@@ -9,3 +8,14 @@ chrome.extension.sendMessage({
     action: "getSource",
     source: get_source(document.body, document)
 });
+
+if(document.getElementById("findInCoupang")){
+	document.getElementById("findInCoupang").remove();
+} else {
+	if(document.getElementsByClassName("loc") && document.getElementsByClassName("loc").length>0){
+		document.getElementsByClassName("loc")[0].innerHTML += 
+			"<span id='findInCoupang' style='padding-left:8px'><a href='http://coupang.com/'>쿠팡에선..?</a></span>";
+	}
+}
+
+
